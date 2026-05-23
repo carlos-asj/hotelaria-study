2c5776993366369d8f3342ab76f28e3898e6cd1e
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2026-05-23 13:57:26 +0000

    feat: add `guests` table on database using migrations

---
+++ infra/migrations/1779542663201_create-hospedes.js
@@ -0,0 +1,50 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("guests", {
+    id: {
+      type: "serial",
+      primaryKey: true,
+    },
+    name: {
+      type: "varchar(100)",
+      notNull: true,
+    },
+    email: {
+      type: "varchar(150)",
+      notNull: true,
+      unique: true,
+    },
+    phone: {
+      type: "varchar(20)",
+      notNull: false,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("gustes");
+};
