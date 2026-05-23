af664a75bdeebc55c387cd5afa79b3d204b8e621
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2026-05-23 13:56:22 +0000

    build: add watch mode for `server.js` using `morgan` tool

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
 import { getStatus } from "./controllers/database.controller.js";
+import guestsRoutes from "./routes/guests.routes.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(morgan("dev"));
 app.use("/api/v1/status", getStatus);
+app.use("/api/v1/guests", guestsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
