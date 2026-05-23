8bb8aebcc8af523d54d29b9582380cda5d2dcb80
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2026-05-23 13:12:48 +0000

    feat: create `api/v1/status` endpoint to see database health

---
+++ src/controllers/database.controller.js
@@ -0,0 +1,34 @@
+import database from "../../infra/database.js";
+
+export async function getStatus(req, res) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
+}
